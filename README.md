837aa2b46795ed9e7cf56dcfb73b79e2a3b9eb59
Ophelie22 <59674073+Ophelie22@users.noreply.github.com>
2021-11-30 14:42:36 +0100

    le travail avait pas ete pushe

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # application-meteo-JS
 
 Nous allons creer une pllication meteo grace à une API
+reprendre nos id dans notre HTML est selectionné l'élement correspondant pour le mettre en place.
+
+On va mettre en place un ecouteur d'evenement  Lorsque l'utilisateur cliquera sur le bouton
+changer de ville. et on ouvrira une fenetre modale
--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 
 //dd0a6c36d5f8c2d5e2fb0e1d6c52669c
-let ville = 'Schiltigheim';
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + ville + '&appid=dd0a6c36d5f8c2d5e2fb0e1d6c52669c&units=metric';
+let ville = "Schiltigheim";
+recupererTemperature(ville);
+// on appelle la fct poiur recupererTEmperature(ville)
+let changerDeVille = document.querySelector('#changer');
+changerDeVille.addEventListener('click', () => {
+ville = prompt("Dans quelle ville souhaitez-vous connaitre la météo ?");
+recupererTemperature(ville);
+});
 
-//console.log(url);
+function recupererTemperature(ville){  
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + 
+ville + '&appid=dd0a6c36d5f8c2d5e2fb0e1d6c52669c&units=metric';
 
 let requete = new XMLHttpRequest();
 requete.open('GET', url); 
 requete.responseType = 'json';
 requete.send(); 
-
+// Dès qu'on reçoit une réponse, cette fonction est executée
 requete.onload = function() {
     if (requete.readyState === XMLHttpRequest.DONE) {
       if (requete.status === 200) {
         let reponse = requete.response; 
-        console.log(reponse);
+        let temperature = reponse.main.temp;// on regarde le json
+        let ville       = reponse.name;
+        //on va regarder notre code HTML est reprendre l'ID
+        document.querySelector('#temperature_label').textContent = temperature;
+        document.querySelector ('#ville').textContent = ville;
+        //console.log(reponse);
 
-        }
-    else{
+        }else{
      alert('Un problème est intervenu, merci de revenir plus tard');
         }
     }
+};
 }
 //Key	Name	
 //dd0a6c36d5f8c2d5e2fb0e1d6c52669c
